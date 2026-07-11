bedc8b0e6aad4bfac7a70c10c4237add5ba82881
Sreya S <ssreya734@gmail.com>
2026-07-11 09:58:09 +0530

    Updated Portfolio

--- script.js
+++ script.js
@@ -1,16 +1,16 @@
-const themeToggle = document.getElementById("theme-toggle");
+// const themeToggle = document.getElementById("theme-toggle");
 
-themeToggle.addEventListener("click", () => {
+// themeToggle.addEventListener("click", () => {
 
-    document.body.classList.toggle("light-mode");
+//     document.body.classList.toggle("light-mode");
 
-    if(document.body.classList.contains("light-mode")){
-        themeToggle.textContent = "☀️";
-    }else{
-        themeToggle.textContent = "🌙";
-    }
+//     if(document.body.classList.contains("light-mode")){
+//         themeToggle.textContent = "☀️";
+//     }else{
+//         themeToggle.textContent = "🌙";
+//     }
 
-});
+// });
 
 const hiddenElements = document.querySelectorAll(".hidden");
 
